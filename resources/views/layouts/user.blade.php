ffe6dd1e4ec4b0269f142eb284fbb642fee4d854
Adri Syahrul <adrydhelfa20@gmail.com>
2022-07-25 09:53:49 +0700

    Update user.blade.php

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -14,10 +14,18 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
     <!-- Font Awesome Icons -->
+    <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
     <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
     <!-- Theme style -->
     <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+    <script>
+        $(document).ready(function() {
+            setInterval(function(){ 
+                $('.notification').css("display", "none");
+            }, 5000);
+        });
+    </script>
 </head>
 
 <body class="hold-transition sidebar-mini">
